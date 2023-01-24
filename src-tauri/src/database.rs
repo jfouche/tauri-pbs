6866159a99d718282a80f2fa6c7fa26cbf33b0af
Jérémie Fouché <jeremie.fouche@gmail.com>
2023-01-24 19:09:03 +0100

    separate item impl

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,18 +1,20 @@
+#![allow(dead_code)]
+
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
     Arc, Mutex,
 };
 
 #[derive(Debug)]
-pub struct Item {
+struct ItemImpl {
     id: usize,
     part_number: String,
     name: String,
 }
 
-impl Item {
+impl ItemImpl {
     fn new(id: usize) -> Self {
-        Item {
+        ItemImpl {
             id,
             part_number: "xxxxxxxx".into(),
             name: String::new(),
@@ -20,19 +22,49 @@ impl Item {
     }
 }
 
+#[derive(Debug)]
+pub struct Item(Arc<Mutex<ItemImpl>>);
+
+impl Item {
+    fn new(id: usize) -> Self {
+        Item(Arc::new(Mutex::new(ItemImpl::new(id))))
+    }
+
+    pub fn set_name(&mut self, name: impl Into<String>) {
+        let mut item = self.0.lock().expect("Item::set_name can't lock item");
+        item.name = name.into();
+    }
+
+    /// Get a copy of the name
+    ///
+    pub fn get_name(&self) -> String {
+        self.0
+            .lock()
+            .expect("Item::get_name can't lock item")
+            .name
+            .clone()
+    }
+}
+
+impl Clone for Item {
+    fn clone(&self) -> Self {
+        Item(Arc::clone(&self.0))
+    }
+}
+
 #[derive(Default)]
 pub struct Database {
     counter: AtomicUsize,
-    items: Arc<Mutex<Vec<Arc<Mutex<Item>>>>>,
+    items: Arc<Mutex<Vec<Item>>>,
 }
 
 impl Database {
-    pub fn create_item(&mut self) -> Arc<Mutex<Item>> {
+    pub fn create_item(&mut self) -> Item {
         let id = self.counter.fetch_add(1, Ordering::Relaxed);
-        let item = Arc::new(Mutex::new(Item::new(id)));
+        let item = Item::new(id);
 
         match self.items.lock() {
-            Ok(mut vec) => vec.push(Arc::clone(&item)),
+            Ok(mut vec) => vec.push(item.clone()),
             Err(err) => {
                 panic!("create_item failed : {}", err)
             }
@@ -58,7 +90,8 @@ mod tests {
     fn update_item() {
         let mut db = Database::default();
         let mut item = db.create_item();
-        let mut item = item.lock().unwrap();
-        (*item).name = "coucou".into();
+        item.set_name("MY_NAME");
+        assert_eq!("MY_NAME", item.get_name());
+        println!("Item : {:?}", item);
     }
 }
